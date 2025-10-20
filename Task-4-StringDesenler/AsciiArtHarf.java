ffa2292a3e67712419777cb894dc3e205d2b87a0
nur <250541076@firat.edu.tr>
2025-10-20 15:04:38 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,12 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+    System.out.println("N    N");
+    System.out.println("NN   N");
+    System.out.println("N N  N");
+    System.out.println("N   NN");
+    System.out.println("N    N");
+    
+    
+    
+    }
+}
