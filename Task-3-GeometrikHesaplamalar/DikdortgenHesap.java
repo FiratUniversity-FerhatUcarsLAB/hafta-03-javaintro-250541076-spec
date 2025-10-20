21fefee7b2e7e4749bebe16a012bb4a554a01367
nur <250541076@firat.edu.tr>
2025-10-20 14:21:23 +0300

    DikdörtgenHesap

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,17 @@
+public class DikdörtgenHesap {
+    public static void main(String[] args) {
+    double en = 4.5;
+    double boy = 7.9;
+    
+    // Alan hesapla
+    double alan = en * boy;
+    
+    // Cevre hesapla
+    double cevre = (en + boy) * 2;
+    
+    // Sonucları hesapla
+    System.out.println("Alan: " + alan);
+    System.out.println("Cevre: " + cevre);
+    
+    }
+}
