351a70e8c3ed53c22799c5415acc14c587aadb13
nur <250541076@firat.edu.tr>
2025-10-20 15:12:55 +0300

    Add HesapOzeti class to display product summary

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,13 @@
+public class HesapOzeti {
+    public static void main(String[] args) {
+    System.out.println("Urun\tMiktar\tFiyat");
+    System.out.println("-------------------");
+    System.out.println("Ekmek\t3\t20 TL");
+    System.out.println("Sut\t1\t45 TL");
+    System.out.println("Elma\t5\t15 TL");
+    System.out.println("Armut\t5\t30 TL");
+    System.out.println("-------------------");
+    
+    
+    }
+}
