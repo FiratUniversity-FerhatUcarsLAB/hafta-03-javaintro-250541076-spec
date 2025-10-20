bbf9632bab77671b03a264f0fb042fa3664e6a81
nur <250541076@firat.edu.tr>
2025-10-20 14:59:39 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+public class IsimKartı {
+    public static void main(String[] args) {
+    String ad = "Nur";
+    String soyad = "Cay";
+    String ogrencino = "250541076";
+    String bolum = "Yazilim Muhendisligi";
+    
+    System.out.println("==================");
+    System.out.println(" OGRENCI BILGILERI ");
+    System.out.println("==================");
+    System.out.println("Ad    :" + ad);
+    System.out.println("Soyad :" + soyad);
+    System.out.println("No :" + ogrencino);
+    System.out.println("Bolum:" + bolum);
+    System.out.println("==================");
+    
+    
+    
+    }
+}
