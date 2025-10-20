98e1e86a735c6b9fccb91049e81e9dc62787e39f
nur <250541076@firat.edu.tr>
2025-10-20 13:43:42 +0300

    JavayaHoşGeldiniz

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -1,10 +1,6 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
-class Main {
+public class JavayaHoşGeldiniz {
     public static void main(String[] args) {
         
-        System.out.println("Try programiz.pro");        
         System.out.println("Java'ya Hoş Geldiniz");        
         System.out.println("Java'ya Hoş Geldiniz");        
         System.out.println("Java'ya Hoş Geldiniz");
