d6f40202e951ebc5ed9a05336a454373a26eb8ce
nur <250541076@firat.edu.tr>
2025-10-20 13:45:02 +0300

    Rename class from Main to Ucmesaj

--- Task-1-HosgeldinMesajlari/UcMesaj.java
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -1,7 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
-class Main {
+public class Ucmesaj {
     public static void main(String[] args) {
         System.out.println("Try programiz.pro");
         System.out.println("Java'ya Hoş Geldiniz");
@@ -11,3 +11,4 @@ class Main {
         System.out.println("Programlama Eğlencelidir");
     }
 }
+
